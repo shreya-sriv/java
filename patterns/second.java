00a9fc01857ce1fc47f1139146ff009a5d6d8d38
Shreya-sriv <shreyas.srivastava2000@gmail.com>
2023-08-10 13:20:50 +0530

    new pattern

--- patterns/second.java
+++ patterns/second.java
@@ -1,18 +1,24 @@
-// import java.io.*;
 import java.util.Scanner;
-class first{
+class second{
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        sc.close();
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<=i;j++)
+            for(int j=n;j>i;j--)
             {
                 System.out.print("*");
             }
-            System.out.print("\n");
+            System.out.println("\n");
         }
     }
 }
+// inverted triangle
+// ****
+
+// ***
+
+// **
+
+// *
